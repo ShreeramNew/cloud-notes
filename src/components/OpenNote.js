b163a4279394ff9292c072dd977834ea7dff0f85
ShreeramNew <shreerambca1@gmail.com>
2024-03-23 15:41:41 +0530

    ref- implemeted axios for fetching

--- src/components/OpenNote.js
+++ src/components/OpenNote.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import "../styles/OpenNote.css"
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
-export default function OpenNote() {
+
+export default async function OpenNote() {
   const param=useParams()
+  const url=`http://localhost:5000/note?id=${param.id}`
+  const data=await axios.get(url)
   return (
     <div id='container'>
-      <h1>Working id:{param.id}</h1>
+      <h1>Content: {data.content} title={data.title}</h1>
     </div>
   )
 }
