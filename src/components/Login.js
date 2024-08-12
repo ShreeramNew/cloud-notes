6dbf1aa2c39ecad393191f5094db68084c7f97da
ShreeramNew <shreerambca1@gmail.com>
2024-08-12 22:20:36 +0530

    feat-implementing authentication feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,66 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/Login.css";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+   let usernameRef = useRef(null);
+   let passwordRef = useRef(null);
+
+   //Cleanup function
+   const cleanupFun = () => {
+      usernameRef.current.value = "";
+      passwordRef.current.value = "";
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      let submittedData = {
+         username: usernameRef.current.value,
+         password: passwordRef.current.value,
+      };
+
+      //Submit data
+      let submitURL = "http://localhost:5000/login";
+      let submitResponse = await fetch(submitURL, {
+         method: "post",
+         headers: {
+            "content-type": "application/json",
+         },
+         body: JSON.stringify(submittedData),
+         credentials: "include",
+      });
+      switch (submitResponse.status) {
+         case 200:
+            alert("Login succefull!");
+            break;
+         case 400:
+            alert("Incorrect username or password");
+            cleanupFun();
+            break;
+         default:
+            alert("Something went wrong!!Please try again");
+            cleanupFun();
+            break;
+      }
+   };
    return (
       <>
-         <form action="">
+         <form onSubmit={handleSubmit} action="">
             <h2>Login</h2>
-            <input type="email" placeholder="Enter your email" required id="email" />
-            <input type="password" placeholder="Enter the password" required id="password" />
+            <input
+               ref={usernameRef}
+               type="text"
+               placeholder="Enter your username"
+               required
+               id="uname"
+            />
+            <input
+               ref={passwordRef}
+               type="password"
+               placeholder="Enter the password"
+               required
+               id="password"
+            />
             <input type="submit" value="Login" />
             <h4>
                Don't you have an account?<Link href="">SignUp</Link>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,19 +1,100 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/Login.css";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+   let usernameRef = useRef(null);
+   let passwordRef = useRef(null);
+   let confirmPasswordRef = useRef(null);
+
+   //Cleanup function
+   const cleanupFun = () => {
+      usernameRef.current.value = "";
+      passwordRef.current.value = "";
+      confirmPasswordRef.current.value = "";
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      let submittedData = {
+         username: usernameRef.current.value,
+         password: passwordRef.current.value,
+         confirmPassword: confirmPasswordRef.current.value,
+      };
+      if (submittedData.password !== submittedData.confirmPassword) {
+         alert("Please confirm the password");
+         cleanupFun();
+         return;
+      }
+
+      //Check if username is already present or not
+      let URL = "http://localhost:5000/getUsers";
+      let response = await fetch(URL);
+      let isUserPresent = false;
+      if (response.status === 200) {
+         let result = await response.json();
+         result.forEach((user) => {
+            if (user.username === submittedData.username) {
+               alert("Sorry!This username is already created!");
+               cleanupFun();
+               isUserPresent = true;
+            }
+         });
+      } else {
+         alert("Sorry!Something went wrong");
+         return;
+      }
+      if (isUserPresent) {
+         return;
+      }
+
+      //If everything is okay, submit the data
+      let submitURL = "http://localhost:5000/signup";
+      let submitResponse = await fetch(submitURL, {
+         method: "post",
+         headers: {
+            "content-type": "application/json",
+         },
+         body: JSON.stringify(submittedData),
+         credentials: "include",
+      });
+      if (submitResponse.status === 200) {
+         alert("Account created succefully!");
+         cleanupFun();
+      } else {
+         alert("Sorry!Something went wrong");
+         cleanupFun();
+      }
+   };
    return (
       <>
-         <form action="">
+         <form onSubmit={handleSubmit} action="">
             <h2>SignUp</h2>
-            <input type="email" placeholder="Enter your email" required id="email" />
-            <input type="password" placeholder="Enter the password" required id="password" />
-            <input type="password" placeholder="Confirm password" required id="conPassword" />
+            <input
+               ref={usernameRef}
+               type="text"
+               placeholder="Enter your username"
+               required
+               id="uname"
+            />
+            <input
+               ref={passwordRef}
+               type="password"
+               placeholder="Enter the password"
+               required
+               id="password"
+            />
+            <input
+               ref={confirmPasswordRef}
+               type="password"
+               placeholder="Confirm password"
+               required
+               id="conPassword"
+            />
 
             <input type="submit" value="SignUp" />
             <h4>
-              Already have an account?<Link href="">Login</Link>
+               Already have an account?<Link href="">Login</Link>
             </h4>
          </form>
       </>
